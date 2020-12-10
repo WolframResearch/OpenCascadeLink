c8c825e9b045b804402b16a8e9a41d711d8c0c27
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2020-12-10 14:22:37 +0100

    Simplifies tests

--- Tests/TestingUtilities.m
+++ Tests/TestingUtilities.m
@@ -1,109 +1,53 @@
 (* Wolfram Language package *)
 
-(* Utility functions *)
-
-Options[maskedQTest] = {"Level" -> 0, "Operation" -> Identity};
-(* "Level": the level at which the qTest should be applied;
-"Operation": The operation applied on the output *)
-
-(* TODO: map out Repeated/RepeatedNull *)
-(* TODO: (package) make into package *)
-
-maskedQTest[qTest_, actual_, expected_, opts : OptionsPattern[]] :=
-    Block[ {level = OptionValue[maskedQTest, opts, "Level"]},
-    	(* Dimension checks *)
-    	If[level > 0 && Length[Level[actual, level]] =!= Length[Level[expected, level]],
-    		(* TODO: (package) put into message  *)
-    		Print["Message: Cannot thread maskedQTest into level " <> ToString[level] 
-    				<> ". Resetting Level to 0."];
-    		level = 0;
-    	];
-        MapThread[maskedQTest[qTest, opts], {actual, expected}, level]
-    ];
- 
-maskedQTest[qTest_, opts : OptionsPattern[]][actual_, expected:List[_RepeatedNull | _Repeated]] :=
-	Block[{expectedPattern, res},
-		If[MatchQ[expected, List[_RepeatedNull]] && SameQ[actual, {}], Return[True]];
-		expectedPattern = First[First[expected]];
-		res = Map[If[qTest[#, expectedPattern], True, #]&, actual];
-		If[TrueQ[And @@ res] && res =!= {},
-			True,
-			<|"actual" -> DeleteCases[res, True],
-			"expected" -> expected|>
-		]
-	]
-
-maskedQTest[qTest_, opts : OptionsPattern[]][actual_, expected_] :=
-	(*Mask matching output*)
-    Block[ {operation = OptionValue[maskedQTest, opts, "Operation"]},
-        If[ qTest[actual, expected],
-            True,
-            <|"actual" -> operation[actual], 
-            "expected" -> operation[expected]|>,
-            {actual, expected}
-        ]
-    ];
-
 boolOperationTest[shapeAssoc_Association, operation_String, bugID_ : ""] :=
-With[{timeConstrain = 20, rOp = Symbol["Region"<>operation], ocOp = Symbol["OpenCascadeShape"<>operation] },
-     Block[ {shape, bmesh, groups, temp, colors, compoundRegion, isEmpty = False, solidQ, res, dim},
+With[
+	{timeConstrain = 20, rOp = Symbol["Region"<>operation], ocOp = Symbol["OpenCascadeShape"<>operation]},
+	{compoundRegion = TimeConstrained[rOp @@ shapeAssoc, timeConstrain]},
+	{If[ Head[ compundRegion] === rOp, compoundRegion = TimeConstrained[rOp @@ (DiscretizeRegion /@ shapeAssoc), timeConstrain]]},
+	{isEmpty = (Head[compoundRegion] === EmptyRegion), dim = TimeConstrained[RegionDimension[compoundRegion], timeConstrain]},
+
+	Block[ {shape, bmesh, groups, temp, colors, result},
+
          Test[
-			shape = CompoundExpression[
-               		compoundRegion = TimeConstrained[rOp @@ #, timeConstrain, $timedOut],
-               		isEmpty = (Head[compoundRegion] === EmptyRegion),
-               		ocOp @@ (OpenCascadeShape /@ #)
-				]&[shapeAssoc];
-             solidQ = If[TrueQ[(dim = TimeConstrained[RegionDimension[compoundRegion], timeConstrain]) <= 2]	
-             			,
-             			<|"RegionDimension" -> dim,
-             			"CompoundQ" -> OpenCascadeShapeType[shape] === "Compound",
-             			"OpenCascadeShapeSolids" -> maskedQTest[SameQ, OpenCascadeShapeSolids[shape], {}],
-             			"OpenCascadeShapeNumberOfSolids" -> maskedQTest[SameQ, OpenCascadeShapeNumberOfSolids[shape], 0]|>
-             			,
-             			(* RegionDimension could return unevaluated.
-             			Assuming the compound region is a solid. *)
-             			<|"RegionDimension" -> dim,
-             			"CompoundQ" -> OpenCascadeShapeType[shape] === "Compound",
-             			"OpenCascadeShapeSolids" -> maskedQTest[MatchQ, OpenCascadeShapeSolids[shape], {_OpenCascadeShapeExpression..}],
- 						"OpenCascadeShapeNumberOfSolids" -> maskedQTest[MatchQ, OpenCascadeShapeNumberOfSolids[shape], _?(# > 0 &)]|>];
-             iPrint["Type check" <> ToString /@ Keys[shapeAssoc]];
-             MatchQ[res = DeleteCases[solidQ, True], <|"RegionDimension" -> _|>] 
-            	|| 
- 						<|"Shapes" -> shapeAssoc,  
- 						"Operation" -> operation,
- 						"Result" -> res|>
-             ,
-             True
-             ,
-             TestID -> "OpenCascadeLink_Docs_BooleanOperations-20200311-"
+			shape = ocOp @@ (OpenCascadeShape /@ shapeAssoc);
+			numOfSolids = OpenCascadeShapeNumberOfSolids[shape];
+			solids = OpenCascadeShapeSolids[shape];
+			bmesh = OpenCascadeShapeSurfaceMeshToBoundaryMesh[shape];
+
+			result = <||>;
+			result["CompoundQ"] = OpenCascadeShapeType[shape] === "Compound";
+
+			(* RegionDimension could have returned unevaluated. Assuming the compound region is a solid. *)
+			result["OpenCascadeShapeNumberOfSolids"] = If[ TrueQ[ dim <= 2],
+				numOfSolids === 0
+			,
+				TrueQ[numOfSolids > 0]
+			];
+
+			result["OpenCascadeShapeSolids"] = If[ TrueQ[ dim <= 2],
+				solids === {}
+			,
+				Union[OpenCascadeShapeExpressionQ/@ solids] === {True}
+			];
+
+			result["ElementMesh"] = If[ isEmpty,
+				bmesh === $Failed
+			,
+				groups = bmesh["BoundaryElementMarkerUnion"];
+				temp = Most[Range[0, 1, 1/(Length[groups])]];
+				colors = ColorData["BrightBands"][#] & /@ temp;
+				Head[bmesh["Wireframe"["MeshElementStyle" -> FaceForm /@ colors]]] === Graphics3D
+			];
+
+			result
+			,
+			<| "CompoundQ" -> True, "OpenCascadeShapeNumberOfSolids" -> True,
+				"OpenCascadeShapeSolids" -> True, "ElementMesh" -> True |>
+			,
+			TestID -> "OpenCascadeLink_Docs_BooleanOperations-20200311-"
               <> operation <> "-ShapeType" <> 
                StringJoin["-" <> ToString[#] & /@ Keys[shapeAssoc]] <> "-bug-401678" <> bugID
-         ];
-         
-         Test[
-         	 (* OpenCascadeShapeSurfaceMeshToBoundaryMesh on Empty regions will return $Failed *)
-         	 bmesh = OpenCascadeShapeSurfaceMeshToBoundaryMesh[shape];
-         	 iPrint["Rendering check" <> ToString /@ Keys[shapeAssoc]];
-         	 {shapeAssoc,
-	         (maskedQTest[SameQ, #[[1]], #[[2]]])& @
-	         If[isEmpty
-	         	 	 ,
-	         	 	 {bmesh, $Failed}
-		             ,
-		             {(* Actual output *)
-		             groups = bmesh["BoundaryElementMarkerUnion"];
-		             temp = Most[Range[0, 1, 1/(Length[groups])]];
-		             colors = ColorData["BrightBands"][#] & /@ temp;
-		             checkGraphicsRendering[Head, bmesh["Wireframe"["MeshElementStyle" -> FaceForm /@ colors]]],
-         	 		 (*Expected output*)
-         	 		 {Graphics3D, "Rendering Errors" -> {}}}]
-		             }
-             ,
-             {shapeAssoc, True}
-             ,
-             TestID -> "OpenCascadeLink_Docs_BooleanOperations-20200311-"
-              <> operation <> "-Rendering" <> 
-               StringJoin["-" <> ToString[#] & /@ Keys[shapeAssoc]] <> bugID <> "-bug-400507-401678"
          ];
      ]
 ]
