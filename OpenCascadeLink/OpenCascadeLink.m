4c0ab16bafe65b8187e325c96e47bbe06b05d6cd
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2021-12-17 13:51:19 +0100

    Adds (Filled)Torus primitive...
    This now works:
     OpenCascadeShape[Torus[]]

--- OpenCascadeLink/OpenCascadeLink.m
+++ OpenCascadeLink/OpenCascadeLink.m
@@ -592,6 +592,17 @@ OpenCascadeShape[OpenCascadeTorus[{p1_, p2_}, r1_, r2_]] :=
 OpenCascadeShape[OpenCascadeTorus[{p1_, p2_}, r1_, r2_, angle_]] :=
 	OpenCascadeShape[OpenCascadeTorus[{p1, p2}, r1, r2, angle, {0, 2 Pi}]]
 
+
+OpenCascadeShape[(FilledTorus|Torus)[]] :=
+	OpenCascadeShape[OpenCascadeTorus[{{0, 0, 0}, {0, 0, 1}}, 1, 1/2]]
+
+OpenCascadeShape[(FilledTorus|Torus)[p1_, r1_, r2_]
+	] /;
+		VectorQ[p1, NumericQ] && (Length[ p1] == 3) &&
+		NumericQ[r1] && NumericQ[r2] && (r1 < r2) :=
+	OpenCascadeShape[OpenCascadeTorus[{p1, p1 + {0, 0, 1}}, r2, r1]]
+
+
 (**)
 (* derived 3D primitives *)
 (**)
