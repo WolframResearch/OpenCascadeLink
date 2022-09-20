9964eb21a26404e13d6f592c723fe0f1e2b8ac6f
Ying Yin <yingy@wolfram.com>
2022-09-20 09:05:03 -0400

    Adding a check for when one of the shapes in boolean operation testing fails to be converted

--- Tests/TestingUtilities.m
+++ Tests/TestingUtilities.m
@@ -14,37 +14,47 @@ boolOperationTest[shapeAssoc_Association, operation_String, bugID_ : ""] :=
                    dim,
                    Min[RegionDimension /@ shapeAssoc]
                ]},
-        Block[ {shape, bmesh, groups, temp, colors, result, head, solids},
-            Test[
-                shape = ocOp @@ (OpenCascadeShape /@ shapeAssoc);
+        Block[ {openCascadeShapes, shape, bmesh, groups, temp, colors, result, head, solids},
+            TestMatch[
+            	openCascadeShapes = OpenCascadeShape /@ shapeAssoc;
+                shape = ocOp[Sequence @@ openCascadeShapes];
                 numOfSolids = OpenCascadeShapeNumberOfSolids[shape];
                 solids = OpenCascadeShapeSolids[shape];
                 bmesh = OpenCascadeShapeSurfaceMeshToBoundaryMesh[shape];
-                result = <||>;
-                result["CompoundQ"] = OpenCascadeShapeType[shape] === "Compound";
-
-                (* RegionDimension could return unevaluated. Assuming the compound region is a solid. *)
-                result["OpenCascadeShapeNumberOfSolids"] = If[ TrueQ[ dim <= 2] || isEmpty,
-                                                               numOfSolids === 0 || <|"Actual" -> numOfSolids, "Expected" -> 0|>,
-                                                               TrueQ[numOfSolids > 0] || <|"Actual" -> numOfSolids, "Expected" -> "Greater than 0"|>
-                                                           ];
-                result["OpenCascadeShapeSolids"] = If[ TrueQ[ dim <= 2],
-                                                       solids === {} || <|"Actual" -> solids, "Expected" -> {}|>,
-                                                       AllTrue[solids, OpenCascadeShapeExpressionQ] === True ||
-                                                       <|"Actual" -> solids, "Expected" -> "AllTrue[solids, OpenCascadeShapeExpressionQ]"|>
-                                                   ];
-                result["ElementMesh"] = If[ isEmpty,
-                                            bmesh === $Failed || <|"Actual" -> bmesh, "Expected" -> $Failed|>,
-                                            groups = bmesh["BoundaryElementMarkerUnion"];
-                                            temp = Most[Range[0, 1, 1/(Length[groups])]];
-                                            colors = ColorData["BrightBands"][#] & /@ temp;
-                                            (head = Head[bmesh["Wireframe"["MeshElementStyle" -> FaceForm /@ colors]]]) === Graphics3D
-                                            ||
-                                            <|"Actual" -> head, "Expected" -> Graphics3D|>
-                                        ];
-                result
+                
+                If[!FreeQ[openCascadeShapes, $Failed], 
+                	(* If one of the shapes fails to convert, check if subsequent operations return unevaluated *)
+                	(MatchQ[shape, _ocOp] && Head[bmash] === OpenCascadeShapeSurfaceMeshToBoundaryMesh) ||
+                	<|"Error" -> "One of the shapes fails to convert. Expecting OpenCascadeShape and OpenCascadeShapeSurfaceMeshToBoundaryMesh to fail",
+                	"OpenCascadeShape" -> shape,
+                	"OpenCascadeShapeSurfaceMeshToBoundaryMesh" -> bmesh|>
+                	,				
+                
+	                result = <||>;
+	                result["CompoundQ"] = OpenCascadeShapeType[shape] === "Compound";
+	
+	                (* RegionDimension could return unevaluated. Assuming the compound region is a solid. *)
+	                result["OpenCascadeShapeNumberOfSolids"] = If[ TrueQ[ dim <= 2] || isEmpty,
+	                                                               numOfSolids === 0 || <|"Actual" -> numOfSolids, "Expected" -> 0|>,
+	                                                               TrueQ[numOfSolids > 0] || <|"Actual" -> numOfSolids, "Expected" -> "Greater than 0"|>
+	                                                           ];
+	                result["OpenCascadeShapeSolids"] = If[ TrueQ[ dim <= 2],
+	                                                       solids === {} || <|"Actual" -> solids, "Expected" -> {}|>,
+	                                                       AllTrue[solids, OpenCascadeShapeExpressionQ] === True ||
+	                                                       <|"Actual" -> solids, "Expected" -> "AllTrue[solids, OpenCascadeShapeExpressionQ]"|>
+	                                                   ];
+	                result["ElementMesh"] = If[isEmpty,
+	                                            bmesh === $Failed || <|"Actual" -> bmesh, "Expected" -> $Failed|>,
+	                                            groups = bmesh["BoundaryElementMarkerUnion"];
+	                                            temp = Most[Range[0, 1, 1/(Length[groups])]];
+	                                            colors = ColorData["BrightBands"][#] & /@ temp;
+	                                            (head = Head[bmesh["Wireframe"["MeshElementStyle" -> FaceForm /@ colors]]]) === Graphics3D
+	                                            ||
+	                                            <|"Actual" -> head, "Expected" -> Graphics3D|>
+	                                        ];
+	                result]
                 ,
-                <| "CompoundQ" -> True, "OpenCascadeShapeNumberOfSolids" -> True,
+                True | <| "CompoundQ" -> True, "OpenCascadeShapeNumberOfSolids" -> True,
                     "OpenCascadeShapeSolids" -> True, "ElementMesh" -> True |>
                 ,
                 TestID -> "OpenCascadeLink_Docs_BooleanOperations-20200311-"
